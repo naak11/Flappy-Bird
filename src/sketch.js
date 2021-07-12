40f013079108c175c4615f5672be0a5edb438d4c
Naheek <65249340+naak11@users.noreply.github.com>
2021-07-12 23:12:25 +0500

    Add files via upload

---
+++ src/sketch.js
@@ -0,0 +1,25 @@
+let bird;
+let lastFrame = 0;
+let pipes = [];
+let c = false;
+let score = 0;
+function setup() {
+    createCanvas(400, 600)
+    bird = new Bird(20)
+}
+
+function draw() {
+    if ((done() || c) && lastFrame == frameCount - 30) {
+        endGame();
+    } else if (!bird.checkEdges() && !c) {
+        playGame();
+    }
+}
+
+
+function keyPressed() {
+    bird.jump();
+    if(key == 'r' || key == "R"){
+        window.location.reload()
+    }
+}
